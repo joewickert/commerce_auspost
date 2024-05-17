31e3ae256de53f0bdf83fd78175dc3d1f5598948
joewickert <joe@summerhill.com.au>
2024-05-17 15:20:05 +1000

    add methods and switch to drupal event dispatcher

--- src/Event/BeforePackEvent.php
+++ src/Event/BeforePackEvent.php
@@ -3,10 +3,12 @@
 namespace Drupal\commerce_auspost\Event;
 
 use Drupal\commerce_order\Entity\OrderItemInterface;
+use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_price\Price;
 use Drupal\commerce_shipping\Entity\ShipmentInterface;
 use Drupal\commerce_shipping\ShippingRate;
-use Symfony\Component\EventDispatcher\Event;
+use Drupal\profile\Entity\ProfileInterface;
+use Drupal\Component\EventDispatcher\Event;
 
 /**
  * Defines the commerce auspost before pack event.
